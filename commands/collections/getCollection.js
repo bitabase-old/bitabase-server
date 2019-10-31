e84c6aedc6b5155db182bc29829d7a7d59795ff2
Mark Wylde <me@markwylde.co.uk>
2019-10-31 23:26:53 +1000

    More beautiful linting

--- commands/collections/getCollection.js
+++ commands/collections/getCollection.js
@@ -4,29 +4,27 @@ const path = require('path')
 const access = promisify(fs.access)
 const readFile = promisify(fs.readFile)
 
-module.exports = function (account, collectionId) {
-  return new Promise(async (resolve, reject) => {
-    if (account.match(/[^a-z0-9]/gi, '')) {
-      console.log('Invalid subdomain')
-      return reject({ code: 404 })
-    }
+module.exports = async function (account, collectionId) {
+  if (account.match(/[^a-z0-9]/gi, '')) {
+    console.log('Invalid subdomain')
+    throw { code: 404 }
+  }
 
-    if (collectionId.match(/[^a-z0-9]/gi, '')) {
-      console.log('Invalid collection ID')
-      return reject({ code: 404 })
-    }
+  if (collectionId.match(/[^a-z0-9]/gi, '')) {
+    console.log('Invalid collection ID')
+    throw { code: 404 }
+  }
 
-    const configFile = path.resolve(__dirname, `../../data/${account}/${collectionId}`)
+  const configFile = path.resolve(__dirname, `../../data/${account}/${collectionId}`)
 
-    try {
-      await access(configFile + '.json', fs.constants.F_OK)
-    } catch (err) {
-      return reject({ code: 404 })
-    }
+  try {
+    await access(configFile + '.json', fs.constants.F_OK)
+  } catch (err) {
+    throw { code: 404 }
+  }
 
-    let config = await readFile(configFile + '.json', 'utf8')
-    config = JSON.parse(config)
+  let config = await readFile(configFile + '.json', 'utf8')
+  config = JSON.parse(config)
 
-    resolve({ config, configFile, account, collectionId })
-  })
+  return { config, configFile, account, collectionId }
 }
--- commands/collections/getUser.js
+++ commands/collections/getUser.js
@@ -4,25 +4,21 @@ function bcryptCompare (text, hash) {
   return bcrypt.compare(text, hash)
 }
 
-module.exports = function (db, username, password) {
-  return new Promise(async (resolve, reject) => {
-    // Get user if logged in
-    if (username && password) {
-      let user = await db.all(
-        'SELECT * FROM users WHERE username = ?',
-        [username]
-      )
-      user = user[0]
+module.exports = async function (db, username, password) {
+  // Get user if logged in
+  if (username && password) {
+    let user = await db.all(
+      'SELECT * FROM users WHERE username = ?',
+      [username]
+    )
+    user = user[0]
 
-      const passwordMatched = await bcryptCompare(password, user.password)
+    const passwordMatched = await bcryptCompare(password, user.password)
 
-      if (user && passwordMatched) {
-        resolve(user)
-      } else {
-        reject({ code: 401, friendly: 'incorrect username and password' })
-      }
+    if (user && passwordMatched) {
+      return user
     } else {
-      resolve()
+      throw { code: 401, friendly: 'incorrect username and password' }
     }
-  })
+  }
 }
