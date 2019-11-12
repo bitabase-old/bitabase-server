b78dfecf9c4dd4b74d800efd93c0f4bcd9f41792
Kory Nunn <knunn187@gmail.com>
2019-11-12 17:20:48 +1000

    Exampe in righto

--- commands/collections/create.js
+++ commands/collections/create.js
@@ -1,8 +1,7 @@
 const { promisify } = require('util')
 const fs = require('fs')
 const path = require('path')
-
-const writeFile = promisify(fs.writeFile)
+const righto = require('righto')
 
 const validate = require('./validate')
 const connect = require('../../modules/db')
@@ -32,44 +31,78 @@ async function getConfig (filename) {
   })
 }
 
-module.exports = async function (req, res, params) {
-  const data = await parseJsonBody(req)
+function createCollectionConfig(databaseName, schema, callback){
+  const databaseDirectory = path.resolve(__dirname, '../../data', `${params.databaseName}`)
+  const configFilePath = path.join(databaseDirectory, `${schema.id}.json`)
+  const databaseFilePath = `${params.databaseName}/${data.id}.db`
 
-  // Validation
-  const errors = validate(data)
-  if (errors) {
-    return sendError(422, { errors }, res)
+  return {
+    databaseDirectory,
+    configFilePath,
+    databaseFilePath
   }
+}
 
-  // Configuration
-  const configFile = path.resolve(__dirname, '../../data', `${params.databaseName}/${data.id}.json`)
+function createCollection(databaseName, collectionDefinition, callback){
+  const collectionConfig = createCollectionConfig(databaseName, collectionDefinition)
+  const configFileStat = righto(fs.stat, collectionConfig.configFilePath)
+  const databaseAvailable = righto.handle(configFileStat, (error, callback) => {
+    callback(error.code === 'ENOENT' ? null : error)
+  })
+  const directoryExists = righto(ensureDirectoryExists, collectionConfig.databaseDirectory, { resolve: true })
+  const configFileWritten = righto(fs.writeFile, createCollection.configFilePath, JSON.stringify(collectionDefinition), 'utf8', righto.after(directoryExists))
+  const availableConfig = righto.mate(collectionConfig, righto.after(configFileWritten))
 
-  await ensureDirectoryExists(configFile, { resolve: true })
+  availableConfig(callback)
+}
 
-  const existingConfig = await getConfig(configFile)
-  if (existingConfig) {
-    return sendError(422, { errors: { id: 'already taken' } }, res)
-  }
+function closeDb(db, callback){
+  const closed = righto.from(db.close())
 
-  await writeFile(configFile, JSON.stringify(data))
+  closed(callback)
+}
 
-  // Create db
-  const db = await connect(`${params.databaseName}/${data.id}.db`)
-
-  const fields = Object.keys(data.schema || [])
-    .map(fieldKey => {
-      return `${fieldKey} TEXT`
+function respond(error, result){
+  if(error){
+    // Respond
+    res.writeHead(500, {
+      'Content-Type': 'application/json'
     })
-    .join(', ')
-
-  const idField = 'id VARCHAR (36) PRIMARY KEY NOT NULL UNIQUE'
-
-  await db.run(`CREATE TABLE ${data.id} (${idField} ${fields ? ', ' + fields : ''})`)
-  await db.close()
-
+    res.end(JSON.stringify(error))
+    return
+  }
   // Respond
   res.writeHead(201, {
     'Content-Type': 'application/json'
   })
-  res.end(JSON.stringify(data))
+  res.end(JSON.stringify(result))
+}
+
+function getDbFields(collectionDefinition){
+  return Object.keys(collectionDefinition.schema || [])
+    .map(fieldKey => {
+      return `${fieldKey} TEXT`
+    })
+    .join(', ')
+}
+
+function createTable(db, collectionDefinition, callback){
+  const idField = 'id VARCHAR (36) PRIMARY KEY NOT NULL UNIQUE'
+  const fields = getDbFields(collectionDefinition)
+  const tableCreated = righto.from(db.run(`CREATE TABLE ${collectionDefinition.id} (${idField} ${fields ? ', ' + fields : ''})`))
+
+  tableCreated(callback)
+}
+
+module.exports = async function (req, res, params) {
+  const databaseName = params.databaseName
+  const collectionDefinition = righto.from(parseJsonBody(req))
+  const validCollectionDefinition = righto(validate, collectionDefinition)
+  const availableConfig = righto(createCollection, validCollectionDefinition)
+  const db = righto.sync(connect, availableConfig.get('databaseFilePath'))
+  const tableCreated = righto(createTable, db, validCollectionDefinition)
+  const dbClosed = righto(closeDb, db, righto.after(tableCreated))
+  const result = righto.mate(validCollectionDefinition, righto.after(dbClosed))
+
+  result(respond)
 }
