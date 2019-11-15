f8cd1e79f83dc1f0405b17a1b01bc2a4b33fa915
Mark Wylde <me@markwylde.co.uk>
2019-11-16 07:29:03 +1000

    Add environment configs

--- config/index.js
+++ config/index.js
@@ -1,3 +1,11 @@
-module.exports = {
-  port: 10000
+const config = {
+  dev: {
+    port: 8000
+  },
+
+  production: {
+    port: 10000
+  }
 }
+
+module.exports = config[process.env.NODE_ENV || 'dev']
