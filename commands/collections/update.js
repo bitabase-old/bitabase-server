f3f4f4b0116930c5408ccd84380170d5307e4a4a
Mark Wylde <me@markwylde.co.uk>
2019-11-22 19:38:08 +1000

    Lintingg=

--- commands/collections/update.js
+++ commands/collections/update.js
@@ -1,4 +1,3 @@
-const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
@@ -13,16 +12,16 @@ const connectWithCreate = require('../../modules/connectWithCreate');
 function getExistingFieldNames (id, dbConnection, callback) {
   sqlite.getAll(`PRAGMA table_info(${id})`, dbConnection, function (error, existingFields) {
     if (error) {
-      return callback(error)
+      return callback(error);
     }
 
     existingFields = existingFields.map(field => field.name);
     callback(null, existingFields);
-  })
+  });
 }
 
 function getConfigFilePath (databasePath, databaseName, collectionId) {
-  return path.resolve(databasePath, `${databaseName}/${collectionId}.json`)
+  return path.resolve(databasePath, `${databaseName}/${collectionId}.json`);
 }
 
 function cleanDataFromDeletedColumns (existingFields, data, dbConnection, callback) {
@@ -31,10 +30,10 @@ function cleanDataFromDeletedColumns (existingFields, data, dbConnection, callba
     .map(field => `${field}=''`);
 
   if (fieldsToDelete.length > 0) {
-    return sqlite.run(`UPDATE ${data.id} SET ${fieldsToDelete.join(', ')}`, dbConnection, callback)
+    return sqlite.run(`UPDATE ${data.id} SET ${fieldsToDelete.join(', ')}`, dbConnection, callback);
   }
 
-  callback()
+  callback();
 }
 
 function addNewColumnsToCollection (existingFields, data, dbConnection, callback) {
@@ -44,56 +43,50 @@ function addNewColumnsToCollection (existingFields, data, dbConnection, callback
       return righto(sqlite.run, `ALTER TABLE ${data.id} ADD ${field} TEXT`, dbConnection);
     });
 
-  righto.all(fieldsToAdd)(function (error, results) {
-    if (error) {
-      return callback(error)
-    }
-
-    callback()
-  })
+  righto.all(fieldsToAdd)(callback);
 }
 
-function updateConfigFile (databasePath, databaseName, validData, callback) {
-  const configFile = righto.sync(getConfigFilePath, databasePath, databaseName, validData.id)
+function updateConfigFile (databasePath, databaseName, data, callback) {
+  const configFile = righto.sync(getConfigFilePath, databasePath, databaseName, data.id);
   const configFolder = righto.sync(path.dirname, configFile);
 
   const folderExists = righto(mkdirp, configFolder);
   const existingConfigFile = righto(fs.stat, configFile, righto.after(folderExists));
 
-  const writtenConfigFile = righto(fs.writeFile, configFile, JSON.stringify(validData), righto.after(existingConfigFile))
+  const writtenConfigFile = righto(fs.writeFile, configFile, JSON.stringify(data), righto.after(existingConfigFile));
 
-  writtenConfigFile(callback)
+  writtenConfigFile(callback);
 }
 
-function syncTableFields (validData, dbConnection, callback) {
-  const existingFields = righto(getExistingFieldNames, validData.id, dbConnection)
-  const cleanedColumns = righto(cleanDataFromDeletedColumns, existingFields, validData, dbConnection);
-  const insertedColumns = righto(addNewColumnsToCollection, existingFields, validData, dbConnection);
+function syncTableFields (data, dbConnection, callback) {
+  const existingFields = righto(getExistingFieldNames, data.id, dbConnection);
+  const cleanedColumns = righto(cleanDataFromDeletedColumns, existingFields, data, dbConnection);
+  const insertedColumns = righto(addNewColumnsToCollection, existingFields, data, dbConnection);
 
-  righto.mate(insertedColumns, cleanedColumns)(callback)
+  righto.mate(insertedColumns, cleanedColumns)(callback);
 }
 
 module.exports = appConfig => function (request, response, params) {
   const parsedData = righto(finalStream, request, JSON.parse);
-  const validData = righto(validate, parsedData).get(data => ({
+  const data = righto(validate, parsedData).get(data => ({
     ...data,
     id: params.collectionId
   }));
 
-  const updatedConfigFile = righto(updateConfigFile, appConfig.databasePath, params.databaseName, validData)
+  const updatedConfigFile = righto(updateConfigFile, appConfig.databasePath, params.databaseName, data);
 
-  const dbFilePath = righto.sync(path.resolve, appConfig.databasePath, params.databaseName, validData.get(data => `${data.id}.db`), righto.after(updatedConfigFile));
+  const dbFilePath = righto.sync(path.resolve, appConfig.databasePath, params.databaseName, data.get(data => `${data.id}.db`), righto.after(updatedConfigFile));
   const dbConnection = righto(connectWithCreate, dbFilePath);
 
-  const syncedTableFields = righto(syncTableFields, validData, dbConnection)
-  const closedDatabase = righto(sqlite.close, dbConnection, righto.after(syncedTableFields))
+  const syncedTableFields = righto(syncTableFields, data, dbConnection);
+  const closedDatabase = righto(sqlite.close, dbConnection, righto.after(syncedTableFields));
 
   closedDatabase(function (error, data) {
     if (error) {
       console.log(error);
-      return writeResponse(500, 'Unexpected Server Error', response);  
+      return writeResponse(500, 'Unexpected Server Error', response);
     }
 
     writeResponse(200, data, response);
-  })
+  });
 };
