77378914ca6ff1090780fa645c0929b2af0ab63e
Mark Wylde <me@markwylde.co.uk>
2019-11-26 22:04:14 +1000

    Present numbers as numbers

--- test/records/read.js
+++ test/records/read.js
@@ -37,3 +37,38 @@ test('arrays are returned as arrays', async t => {
   t.equal(testRead.status, 200);
   t.deepEqual(testRead.data.test, ['something']);
 });
+
+test('numbers are returned as numbers', async t => {
+  t.plan(4);
+
+  await reset();
+
+  const server = await createServer().start();
+
+  await httpRequest('/v1/databases/test/collections', {
+    method: 'post',
+    data: {
+      name: 'tests',
+      schema: {
+        test: ['required', 'number']
+      }
+    }
+  });
+
+  const testInsert = await httpRequest('/v1/databases/test/records/tests', {
+    method: 'post',
+    data: {
+      test: 100
+    }
+  });
+
+  const testRead = await httpRequest(`/v1/databases/test/records/tests/${testInsert.data.id}`);
+
+  await server.stop();
+
+  t.equal(testInsert.status, 201);
+  t.deepEqual(testInsert.data.test, 100);
+
+  t.equal(testRead.status, 200);
+  t.deepEqual(testRead.data.test, 100);
+});
