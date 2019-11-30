6655ae8b7fd521cabd704c06e91011f438952fb9
Mark Wylde <me@markwylde.co.uk>
2019-11-30 21:02:20 +1000

    Move cachableSqlite to rightotest

--- test/modules/cachableSqlite.js
+++ test/modules/cachableSqlite.js
@@ -2,101 +2,99 @@ const path = require('path');
 const righto = require('righto');
 const callarestJson = require('callarest/json');
 
-const config = require('../../config');
-const test = require('tape');
-const reset = require('../helpers/reset');
+const reset = require('../helpers/resetCB');
 const createServer = require('../../server');
+const rightoTest = require('../helpers/rightoTest');
 
+const config = require('../../config');
 const cachableSqlite = require('../../modules/cachableSqlite');
 
 const sleep = (ms, callback) => setTimeout(callback, ms);
 
-test('database stays open', async t => {
+rightoTest('database stays open', function * (t) {
   t.plan(5);
 
-  await reset();
+  yield righto(reset);
+  const server = createServer();
+  yield righto(server.start);
 
-  const server = await createServer().start();
   const dbFile = path.resolve(config.databasePath, 'test/users.db');
 
-  righto.iterate(function * () {
-    yield righto(callarestJson, {
-      url: 'http://localhost:8000/v1/databases/test/collections',
-      method: 'post',
-      data: {
-        name: 'users',
-        schema: {
-          test: ['required', 'array']
-        }
+  yield righto(callarestJson, {
+    url: 'http://localhost:8000/v1/databases/test/collections',
+    method: 'post',
+    data: {
+      name: 'users',
+      schema: {
+        test: ['required', 'array']
       }
-    });
+    }
+  });
 
-    const firstRead = yield righto(callarestJson, {
-      url: 'http://localhost:8000/v1/databases/test/records/users/nothere',
-      method: 'get'
-    });
+  const firstRead = yield righto(callarestJson, {
+    url: 'http://localhost:8000/v1/databases/test/records/users/nothere',
+    method: 'get'
+  });
 
-    const firstConnection = yield righto(cachableSqlite.getConnection, dbFile);
+  const firstConnection = yield righto(cachableSqlite.getConnection, dbFile);
 
-    yield righto(sleep, 1500);
+  yield righto(sleep, 1500);
 
-    const secondRead = yield righto(callarestJson, {
-      url: 'http://localhost:8000/v1/databases/test/records/users/nothere'
-    });
+  const secondRead = yield righto(callarestJson, {
+    url: 'http://localhost:8000/v1/databases/test/records/users/nothere'
+  });
 
-    const secondConnection = yield righto(cachableSqlite.getConnection, dbFile);
+  const secondConnection = yield righto(cachableSqlite.getConnection, dbFile);
 
-    server.stop();
+  server.stop();
 
-    t.equal(firstRead.response.statusCode, 404);
-    t.deepEqual(firstRead.body, { error: 'Not Found' });
-    t.equal(secondRead.response.statusCode, 404);
-    t.deepEqual(secondRead.body, { error: 'Not Found' });
-    t.notEqual(firstConnection.timeOpened, secondConnection.timeOpened);
-  })(error => error && console.log(error));
+  t.equal(firstRead.response.statusCode, 404);
+  t.deepEqual(firstRead.body, { error: 'Not Found' });
+  t.equal(secondRead.response.statusCode, 404);
+  t.deepEqual(secondRead.body, { error: 'Not Found' });
+  t.notEqual(firstConnection.timeOpened, secondConnection.timeOpened);
 });
 
-test('database stays open', async t => {
+rightoTest('database stays open', function * (t) {
   t.plan(5);
 
-  await reset();
+  yield righto(reset);
+  const server = createServer();
+  yield righto(server.start);
 
-  const server = await createServer().start();
   const dbFile = path.resolve(config.databasePath, 'test/users.db');
 
-  righto.iterate(function * () {
-    yield righto(callarestJson, {
-      url: 'http://localhost:8000/v1/databases/test/collections',
-      method: 'post',
-      data: {
-        name: 'users',
-        schema: {
-          test: ['required', 'array']
-        }
+  yield righto(callarestJson, {
+    url: 'http://localhost:8000/v1/databases/test/collections',
+    method: 'post',
+    data: {
+      name: 'users',
+      schema: {
+        test: ['required', 'array']
       }
-    });
+    }
+  });
 
-    const firstRead = yield righto(callarestJson, {
-      url: 'http://localhost:8000/v1/databases/test/records/users/nothere',
-      method: 'get'
-    });
+  const firstRead = yield righto(callarestJson, {
+    url: 'http://localhost:8000/v1/databases/test/records/users/nothere',
+    method: 'get'
+  });
 
-    const firstConnection = yield righto(cachableSqlite.getConnection, dbFile);
+  const firstConnection = yield righto(cachableSqlite.getConnection, dbFile);
 
-    yield righto(sleep, 500);
+  yield righto(sleep, 500);
 
-    const secondRead = yield righto(callarestJson, {
-      url: 'http://localhost:8000/v1/databases/test/records/users/nothere'
-    });
+  const secondRead = yield righto(callarestJson, {
+    url: 'http://localhost:8000/v1/databases/test/records/users/nothere'
+  });
 
-    const secondConnection = yield righto(cachableSqlite.getConnection, dbFile);
+  const secondConnection = yield righto(cachableSqlite.getConnection, dbFile);
 
-    server.stop();
+  server.stop();
 
-    t.equal(firstRead.response.statusCode, 404);
-    t.deepEqual(firstRead.body, { error: 'Not Found' });
-    t.equal(secondRead.response.statusCode, 404);
-    t.deepEqual(secondRead.body, { error: 'Not Found' });
-    t.equal(firstConnection.timeOpened, secondConnection.timeOpened);
-  })(error => error && console.log(error));
+  t.equal(firstRead.response.statusCode, 404);
+  t.deepEqual(firstRead.body, { error: 'Not Found' });
+  t.equal(secondRead.response.statusCode, 404);
+  t.deepEqual(secondRead.body, { error: 'Not Found' });
+  t.equal(firstConnection.timeOpened, secondConnection.timeOpened);
 });
